e3d92b5e4a4ad8d834f9e2fa0c51b3d08ef1f781
shishir-m <75567472+shishir-m@users.noreply.github.com>
2022-04-28 12:50:30 +0530

    Add files via upload

---
+++ src/GUI/EnterDetailsFactory.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package university.management.system.GUI;
+import java.awt.event.*;
+
+public interface EnterDetailsFactory extends ActionListener{
+    void createMember(ActionEvent ae);
+    
+}

6eafe171a076c1d1b46b356937510009d011a203
shishir-m <75567472+shishir-m@users.noreply.github.com>
2022-04-28 12:48:21 +0530

    Create Main.java

---
+++ src/controller/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package university.management.system.Controller;
+import university.management.system.GUI.Project;
+
+/**
+ *
+ * @author Shreesh
+ */
+public class Main{ 
+    public static void main(String[] args) {
+        Project main_proj = new Project();
+        main_proj.setVisible(true);
+    }
+}
